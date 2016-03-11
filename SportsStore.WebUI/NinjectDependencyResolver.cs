234fc89f28fbea4d72fba804bb61f5744ad0c608
Yoga <yoga.adi21@gmail.com>
2016-03-11 17:09:58 +0700

    inisial komit

---
+++ SportsStore.WebUI/NinjectDependencyResolver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Linq;
+using Moq;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        public IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviveType)
+        {
+            return kernel.GetAll(serviveType);
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind<IProductsRepository>().To<EFProductRepository>();
+        }
+    }
+}
